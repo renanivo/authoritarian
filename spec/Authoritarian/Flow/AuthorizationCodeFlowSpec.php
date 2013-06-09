e6fe3b6a18d87128035e7b38838d710f2b1cd3d4
Renan Ivo <renanivom@gmail.com>
2013-06-09 18:27:15 -0300

    create getAuthorizeUrl in AuthorizationCodeFlow class

--- spec/Authoritarian/Flow/AuthorizationCodeFlowSpec.php
+++ spec/Authoritarian/Flow/AuthorizationCodeFlowSpec.php
@@ -5,20 +5,76 @@ namespace spec\Authoritarian\Flow;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+use PhpSpec\Matcher\CallbackMatcher;
+
 class AuthorizationCodeFlowSpec extends ObjectBehavior
 {
+    private $authorizeUrl;
+
     public function let()
     {
+        $this->authorizeUrl = 'http://api.example.com/oauth/authorize';
         $this->beConstructedWith(
             'http://api.example.com/oauth/token',
             'client id',
             'client secret',
             'scope',
-            'http://api.example.com/oauth/authorize'
+            $this->authorizeUrl
         );
+        $this->setClient(new \Guzzle\Http\Client());
     }
+
     public function it_should_be_initializable()
     {
         $this->shouldHaveType('Authoritarian\Flow\AuthorizationCodeFlow');
     }
+
+    public function it_should_get_the_authorize_with_the_given_url()
+    {
+        $this->getAuthorizeUrl('http://example.com/callback')
+            ->shouldStartWith($this->authorizeUrl);
+    }
+
+    public function it_should_get_the_authorize_url_with_callback()
+    {
+        $this->getAuthorizeUrl('http://example.com/callback')
+            ->shouldMatch('/redirect_uri=http%3A%2F%2Fexample.com%2Fcallback/');
+    }
+
+    public function it_should_get_the_authorize_url_with_clent_id()
+    {
+        $this->getAuthorizeUrl('http://example.com/callback')
+            ->shouldMatch('/client_id=client\+id/');
+    }
+
+    public function it_should_get_the_authorize_url_with_state_when_given()
+    {
+        $this->getAuthorizeUrl('http://example.com/callback', 'state')
+            ->shouldMatch('/state=state/');
+    }
+
+    public function it_should_get_a_valid_authorize_url()
+    {
+        $this->getAuthorizeUrl('http://example.com/callback')
+            ->shouldBeAValidUrl();
+    }
+
+    public function getMatchers()
+    {
+        return array(
+            'beAValidUrl' => function($subject) {
+                return false !== filter_var(
+                    $subject,
+                    FILTER_VALIDATE_URL,
+                    array(
+                        'flags' => array(
+                            FILTER_FLAG_PATH_REQUIRED,
+                            FILTER_FLAG_QUERY_REQUIRED,
+                        ),
+                    )
+                );
+            }
+        );
+    }
 }
+
--- src/Authoritarian/Flow/AuthorizationCodeFlow.php
+++ src/Authoritarian/Flow/AuthorizationCodeFlow.php
@@ -6,7 +6,7 @@ namespace Authoritarian\Flow;
  * Implementation of the Authorization Flow Interface to
  * the Authorization Code Flow of Oauth 2
  **/
-class AuthorizationCodeFlow
+class AuthorizationCodeFlow implements AuthorizationFlowInterface
 {
     protected $tokenUrl;
     protected $clientId;
@@ -35,6 +35,47 @@ class AuthorizationCodeFlow
         $this->clientSecret = $client_secret;
         $this->scope = $scope;
         $this->authorizeUrl = $authorize_url;
+    }
+
+    /**
+     * Set HTTP Client
+     *
+     * @param Guzzle\Http\ClientInterface $client An instance of Guzzle Client
+     */
+    public function setClient(\Guzzle\Http\ClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Get the authorization request
+     *
+     * @return Guzzle\Http\Message\Request
+     */
+    public function getRequest()
+    {
+        return $this->client->get();
+    }
+
+    /**
+     * Get the URL to user's authorization
+     *
+     * @param string $callback_url The URL that will handle the authorization code after the user's login
+     * @param string $state A CSRF token
+     * @return string The URL to user's authorization
+     */
+    public function getAuthorizeUrl($callback_url, $state = null)
+    {
+        $query_parameters = array(
+            'redirect_uri' => $callback_url,
+            'client_id' => $this->clientId,
+        );
+
+        if (!is_null($state)) {
+            $query_parameters['state'] = $state;
+        }
+
+        return $this->authorizeUrl . '?' . http_build_query($query_parameters);
     }
 }
 
