2a15e4bd5ee35494313d53a10a73257a8c6e6eaa
Renan Ivo <renanivom@gmail.com>
2013-10-16 18:18:09 -0300

    create ClientCredentialsFlow

---
+++ spec/Authoritarian/Flow/ClientCredentialsFlowSpec.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace spec\Authoritarian\Flow;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ClientCredentialsFlowSpec extends ObjectBehavior
+{
+    private $clientId;
+    private $clientSecret;
+
+    public function let()
+    {
+        $this->clientId = 'client-id';
+        $this->clientSecret = 'client-secret';
+
+        $this->setHttpClient(new \Guzzle\Http\Client());
+        $this->setTokenUrl('http://example.com/oauth/token');
+        $this->setClientCredential($this->clientId, $this->clientSecret);
+    }
+
+    public function it_should_be_initializable()
+    {
+        $this->shouldHaveType('Authoritarian\Flow\ClientCredentialsFlow');
+    }
+
+    public function it_should_get_a_post_request()
+    {
+        $this->getRequest()->getMethod()->shouldBeEqualTo('POST');
+    }
+
+    public function it_should_get_request_with_grant_type()
+    {
+        $this->getRequest()->shouldHavePostParameter(
+            'grant_type',
+            'client_credentials'
+        );
+    }
+
+    public function it_should_get_a_request_with_client_id()
+    {
+        $this->getRequest()->shouldHavePostParameter(
+            'client_id',
+            $this->clientId
+        );
+    }
+
+    public function it_should_get_a_request_with_client_secret()
+    {
+        $this->getRequest()->shouldHavePostParameter(
+            'client_secret',
+            $this->clientSecret
+        );
+    }
+
+    public function getMatchers()
+    {
+        return array(
+            'beAValidUrl' => function ($subject, $flags) {
+                return false !== filter_var(
+                    $subject,
+                    FILTER_VALIDATE_URL,
+                    array(
+                        'flags' => $flags,
+                    )
+                );
+            },
+            'havePostParameter' => function ($subject, $key, $value) {
+                $body = preg_split('/\n\s*\n/', $subject)[1];
+                $parameters = array();
+                parse_str($body, $parameters);
+
+                return array_key_exists($key, $parameters) &&
+                    $parameters[$key] == $value;
+            }
+        );
+    }
+}
---
+++ src/Authoritarian/Flow/ClientCredentialsFlow.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Authoritarian\Flow;
+
+/**
+ * Implementation of Client Credentials Flow
+ */
+class ClientCredentialsFlow extends AbstractFlow
+{
+    const GRANT_TYPE = 'client_credentials';
+
+    public function getRequest()
+    {
+        parent::getRequest();
+
+        return $this->client->post(
+            $this->tokenUrl,
+            null,
+            $this->removeNullItems(
+                array(
+                    'grant_type' => self::GRANT_TYPE,
+                    'client_id' => $this->clientId,
+                    'client_secret' => $this->clientSecret,
+                )
+            )
+        );
+    }
+}
